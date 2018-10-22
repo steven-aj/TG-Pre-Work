7e24aecf645cd0761ae031cf4bda6b4c79631109
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-22 15:29:48 -0600

    Introductions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/What-are-functions.js
@@ -0,0 +1,5 @@
+/*
+  A function, or method, is a block of code that's written to perform the same
+  task as many times as necessary without having to re-write the same
+  instructions over and over again.
+*/
