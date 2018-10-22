0d6be32a4c0f2a5038f7870e9f395a48711d8006
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-22 15:27:05 -0600

    Introductions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/What-are-Conditional-Statements.js
@@ -0,0 +1,14 @@
+/*
+  Conditional statements are blocks of code that allow us to assess the
+  condition of a given value and operate based on that assessment.
+
+  In real world settings, all objects have states they enter depending on
+  conditions required for those states.
+
+  For example, a windmill has two states: dormant and active.
+  The state of the windmill at any given time will depend upon windy
+  conditions.
+
+  NO wind means windmill is 'dormant'
+  Wind means windmill is 'active'
+*/
