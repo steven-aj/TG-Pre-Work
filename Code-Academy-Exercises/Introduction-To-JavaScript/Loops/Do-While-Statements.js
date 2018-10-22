7b3eb79bc9087ca6d6e72085c9eb83d756de721a
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-22 15:38:24 -0600

    Loop control

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,7 @@
+// Write your code below
+var cupsOfSugarNeeded = 1;
+var cupsAdded = 0;
+
+do {
+  cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded)
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-break-keyword.js
@@ -0,0 +1,10 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write you code below
+for(rapperArrayIndex = 0; rapperArrayIndex < rapperArray.length; rapperArrayIndex++) {
+  if (rapperArray[rapperArrayIndex] === 'Notorious B.I.G.') {
+    break;
+  };
+}
+
+console.log("And if you don't know, now you know.");
