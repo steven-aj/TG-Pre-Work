7d5eace8474e37e18fb8a6daa582ba5f716008b8
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-22 15:40:51 -0600

    Delcaring variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable.js
@@ -0,0 +1,13 @@
+/* VAR */
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+
+/* LET */
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+/* CONST */
+const entree = 'Enchiladas';
+console.log(entree);
+//entree = 'Tacos'; <---- Breaks stuff. Constants are just that!
