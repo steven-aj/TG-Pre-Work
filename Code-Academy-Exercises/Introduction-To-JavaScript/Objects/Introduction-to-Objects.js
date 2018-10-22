6762e8560bc508adfa3531dfb5b5950eba5d62b7
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-22 15:37:09 -0600

    Intros

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Introduction-to-Objects.js
@@ -0,0 +1,11 @@
+/*
+  Objects in object-oriented programming (OOP) use models, or classifications,
+  to be defined.
+
+  An object can be of class Table, for example, and have various properties such
+  as:
+    number of legs
+    color
+    material
+    dimensions: {l, w, h}
+*/
