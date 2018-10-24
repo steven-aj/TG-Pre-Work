e812698b1d0695b3b301f1cd7f724a197bd9df15
johnsonstheory <31742073+johnsonstheory@users.noreply.github.com>
2018-10-24 13:10:55 -0600

    Intro to requests

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/Introduction-to-Requests.js
@@ -0,0 +1,10 @@
+/*
+  HTTP requests are a collection of HTTP methods that give us a range of options
+  when handling data from users.
+
+  Four common requests types:
+    GET
+    POST
+    PUT
+    DELETE
+*/
